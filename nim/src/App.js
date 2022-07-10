d9d939ddec65a318674b9c5b2860cdd6bf7a76ef
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-07-10 10:59:20 -0600

    useState

--- nim/src/App.js
+++ nim/src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import {useState} from 'react';
+import Home from './components/Home';
 
 function App() {
+  const [gameDifficulty, setGameDifficulty] = useState([]);
+  const [comOrPlayer, setComORPlayer] = useState([]);
+  const [comDifficulty, setComDifficulty] = useState([]);
+  const [player1Name, setPlayer1Name] = useState('');
+  const [player2Name, setPlayer2Name] = useState('');
+
+
   return (
     <div className="App">
     </div>
