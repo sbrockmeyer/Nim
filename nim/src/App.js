ffee4ed2e9668423b68def21c7ecfb125f00e6fe
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-07-11 10:49:46 -0600

    Update App.js

--- nim/src/App.js
+++ nim/src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 function App() {
   const [gameDifficulty, setGameDifficulty] = useState([]);
   const [comOrPlayer, setComORPlayer] = useState([]);
-  const [comDifficulty, setComDifficulty] = useState([]);
+  // const [comDifficulty, setComDifficulty] = useState([]);
   const [player1Name, setPlayer1Name] = useState('');
   const [player2Name, setPlayer2Name] = useState('');
 
