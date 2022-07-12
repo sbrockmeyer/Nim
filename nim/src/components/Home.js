3a260cec4fce33fcb26a1a6ad8f12c0656351be9
rbenner7 <50886969+rbenner7@users.noreply.github.com>
2022-07-12 10:49:29 -0600

    home

--- nim/src/components/Home.js
+++ nim/src/components/Home.js
@@ -11,9 +11,7 @@ function Home() {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">the rules</Dropdown.Item>        
       </Dropdown.Menu>
     </Dropdown>
     </div>
