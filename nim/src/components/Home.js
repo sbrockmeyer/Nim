13fbe9a2cbdf2fabd5cac2b8d8533eb9e5aabc41
rbenner7 <50886969+rbenner7@users.noreply.github.com>
2022-07-12 11:02:59 -0600

    ahh

--- nim/src/components/Home.js
+++ nim/src/components/Home.js
@@ -7,11 +7,14 @@ function Home() {
   return (
     <div>
       <h1>NIM</h1>
+      <div>
       <DropdownButton id="dropdown-basic-button" title="Dropdown button">
       <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
       <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
       <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
     </DropdownButton>
+      </div>
+    
     </div>
   )
 }
