51961ce03db29fce556dde58cc55f0ca4d1f9182
rbenner7 <50886969+rbenner7@users.noreply.github.com>
2022-07-12 10:37:06 -0600

    bootstrap

--- nim/src/components/Home.js
+++ nim/src/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function Home() {
   return (
+<<<<<<< Updated upstream
     <div>Home
       <button onClick={toggleGameRunning}>play </button>
+=======
+    <div>
+      <h1>NIM</h1>
+      <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+>>>>>>> Stashed changes
     </div>
   )
 }
