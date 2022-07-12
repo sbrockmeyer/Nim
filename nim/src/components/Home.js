13726f74e1711ed8314de008edbbefd5fd78d1ef
rbenner7 <50886969+rbenner7@users.noreply.github.com>
2022-07-12 10:52:35 -0600

    SAKURA

--- nim/src/components/Home.js
+++ nim/src/components/Home.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 function Home() {
   return (
     <div>
       <h1>NIM</h1>
-      <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">the rules</Dropdown.Item>        
-      </Dropdown.Menu>
-    </Dropdown>
+      <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    </DropdownButton>
     </div>
   )
 }
