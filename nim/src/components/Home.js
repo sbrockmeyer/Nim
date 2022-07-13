f916a8b2391c225864ec5db8d9bdb1db95a16373
rbenner7 <50886969+rbenner7@users.noreply.github.com>
2022-07-12 23:36:45 -0600

    attempt

--- nim/src/components/Home.js
+++ nim/src/components/Home.js
@@ -1,19 +1,46 @@
 import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button from 'react-bootstrap/Button';
 
 
 function Home() {
   return (
     <div>
       <h1>NIM</h1>
-      <div>
+      <table>
+        <tbody>
+          <tr>
+            rules could go here
+          </tr>
+          <tr>
+            <td>
+            <Button as="input" type="button" value="Easy" />{' '}
+            </td>
+          </tr>
+          <tr>
+            com diff
+          </tr>
+          <tr>
+            pvp
+            pvc
+          </tr>
+          <tr> 
+            name
+          </tr>
+          <tr> 
+            name
+          </tr>
+        </tbody>
+      </table>
+      {/* <div>
       <DropdownButton id="dropdown-basic-button" title="Dropdown button">
       <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
       <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
       <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
     </DropdownButton>
-      </div>
+      </div> */}
+      
     
     </div>
   )
